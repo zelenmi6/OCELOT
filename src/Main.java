752da5bc439b49c8d4f3c1d4dc81b96e5fb03c69
Fei Fang <karen.feifang@gmail.com>
2015-12-05 18:09:21 -0600

    Modified SQLiteJDBC class to work better with RtfGenerator class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import static com.tutego.jrtf.Rtf.rtf;
+
 import static com.tutego.jrtf.RtfHeader.font;
 import static com.tutego.jrtf.RtfPara.*;
 import static com.tutego.jrtf.RtfText.*;
@@ -6,6 +7,7 @@ import static com.tutego.jrtf.RtfUnit.CM;
 
 import java.awt.Desktop;
 import java.io.*;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,36 +24,24 @@ public class Main {
 
 	public static void main( String... args ) throws IOException
 	  {
-//	    File out = new File( "out-toc.rtf" );
-//
-//	    rtf().headerStyles( RtfHeaderStyle.values() )
-//	         .section( p( "Table of content\n" ),
-//	                   p( tableOfContentsField() ) )
-//	        .section( p( RtfHeaderStyle.HEADER_1, "Style - Header 1" ),
-//	                  p( RtfHeaderStyle.HEADER_2, "Style - Header 2" ),
-//	                  p( RtfHeaderStyle.HEADER_3, "Style - Header 3" ),
-//	                  p( RtfHeaderStyle.HEADER_1, "Style - Header 1" ),
-//	                  p( RtfHeaderStyle.HEADER_2, "Style - Header 2" ) )
-//	    	.out( new FileWriter( out ) );
-//	    try
-//	    {
-//	      Desktop.getDesktop().open( out );
-//	    }
-//	    catch ( IOException e ) { e.printStackTrace(); }
-//	  }
-//		RtfGenerator rtg = new RtfGenerator("./test.rtf");
-//		String sec1 = "aaaaaaaaaaaaaa \t aaaaaaaaaaaaaaaa \n aaaaaaaaaaaaaa";
-//		String sec2 = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
-//		String sec3 = "cccccccccccccccccccccccccccccccccccccccccccc";
-//		rtg.addSection(sec1, RtfHeaderStyle.HEADER_1, "First section", "Random footnote1");
-//		rtg.addSection(sec2, RtfHeaderStyle.HEADER_2, "Second section", "Random footnote2");
-//		rtg.addSection(sec3, RtfHeaderStyle.HEADER_3, "Third section", "Random footnote3");
-//		rtg.outputFile();
-		
+		//String reportDest;
+		//Scanner input = new Scanner(System.in);
+		//System.out.print("Enter Report Destination (use two slashes): ");
+		//reportDest = input.next();
+		//RtfGenerator rtg = new RtfGenerator(reportDest + "\\test.rtf");
+	    
 		SQLiteJDBC db = new SQLiteJDBC();
-//		db.listAllMeta();
-//		db.listMeta(1);
-		db.executeTest(1);
+	 
+		RtfGenerator rtg = new RtfGenerator("src\\ReportArchive\\test.rtf");
+		String sec1 = db.listAllMeta();
+		String sec2 = db.executeTest(1);
+		String sec3 = db.executeTest(2);
+		rtg.addSection(sec1, RtfHeaderStyle.HEADER_1, "First section", "Random footnote1");
+		rtg.addSection(sec2, RtfHeaderStyle.HEADER_2, "Second section", "Random footnote2");
+		rtg.addSection(sec3, RtfHeaderStyle.HEADER_3, "Third section", "Random footnote3");
+		rtg.outputFile();
+		
+		
 	  }
 }
 
--- src/sqlite/SQLiteJDBC.java
+++ src/sqlite/SQLiteJDBC.java
@@ -32,18 +32,21 @@ public class SQLiteJDBC {
 		}
 	}
 	
-	public void listAllMeta() {
+	public String listAllMeta() {
+		String string = ""; 
 		try {
 			ResultSet rs = listAll.executeQuery();
 			while (rs.next()) {
-				System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t"
-									+ rs.getString(3) + "\t" + rs.getString(4) + "\t");
+				string = string + rs.getString(1) + "\t" + rs.getString(2) + "\t"
+						+ rs.getString(3) + "\t" + rs.getString(4) + "\t";
+				System.out.println(string);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("----------------------------------");
+		System.out.println("listAllMeta----------------------------------");
+		return string;
 	}
 	
 	public ResultSet listMeta(int id) {
@@ -54,7 +57,7 @@ public class SQLiteJDBC {
 				System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" +
 									rs.getString(3) + "\t" + rs.getString(4) + "\t");
 			}
-			System.out.println("----------------------------------");
+			System.out.println("listMeta----------------------------------");
 			return rs;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -63,20 +66,23 @@ public class SQLiteJDBC {
 		}
 	}
 	
-	public void executeTest(int id) {
+	public String executeTest(int id) {
+		String string ="";
 		ResultSet metaRs = listMeta(id);
 		try {
 			listQueries.setInt(1, id);
 			ResultSet queriesRs = listQueries.executeQuery();
 			while (queriesRs.next()) {
-				System.out.println(queriesRs.getInt(1) + "\t" + queriesRs.getInt(2) + "\t" +
-						queriesRs.getString(3) + "\t" + queriesRs.getString(4) + "\t" + queriesRs.getString(5));
+				string = string + queriesRs.getInt(1) + "\t" + queriesRs.getInt(2) + "\t" +
+						queriesRs.getString(3) + "\t" + queriesRs.getString(4) + "\t" + queriesRs.getString(5);
+				System.out.println(string);
 			}
-			System.out.println("----------------------------------");
+			System.out.println("executeTest----------------------------------");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return string;
 	}
 }
 
